4953885e393a5bed12f162fc0e121929d4ab9d11
Ahsan Farabi <ahsanfarabi1971@gmail.com>
2024-05-24 23:49:40 +0600

    Commit

---
+++ app.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct blog
+{
+
+    char title[100];
+    char content[500];
+};
+
+struct user
+{
+
+    char username[50];
+    int password;
+};
+
+int main()
+{
+    struct user u1;
+}
