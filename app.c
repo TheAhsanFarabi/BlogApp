539ae57453dd9b4387f91ce90d0a8649c00473a7
Ahsan Farabi <ahsanfarabi1971@gmail.com>
2024-05-25 06:57:01 +0600

    Commit

--- app.c
+++ app.c
@@ -16,7 +16,37 @@ struct user
     int password;
 };
 
-int main()
+void register()
 {
     struct user u1;
+    printf("Type your username: ");
+    scanf("%s\n", &u1.username);
+    printf("Type your password (INT): ");
+    scanf("%d\n", &u1.password);
+}
+
+void login()
+{
+}
+
+void feed()
+{
+}
+
+int main()
+{
+    printf("Welcome to BlogApp!\n");
+    printf("1. Register\n");
+    printf("2. Login\n");
+    printf("Choose option (1/2): ");
+    int option = 0;
+    scanf("%d", &option);
+    if (option == 1)
+    {
+        register();
+    }
+    else
+    {
+        login();
+    }
 }
